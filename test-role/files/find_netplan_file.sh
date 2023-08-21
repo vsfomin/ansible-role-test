cd6a338a1a588b8efd3c404125b4368245fc7e14
vladimir.fomin <vladimir.fomin>
2023-08-22 01:13:26 +0300

    change code

--- test-role/files/find_netplan_file.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-active_interface=$(ip route get 8.8.8.8 | awk 'NR==1 {print $5}')
-readarray -t files <<< $(grep -rw $active_interface /etc/netplan)
-length=${#files[@]}
-for ((i=0; i<$length; i++))
-do
-  echo ${files[$i]}
-done
